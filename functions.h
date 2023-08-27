3282ced017efbb212e2e2af4edfb653f1c51565f
Vipul <vipulnayak1192@gmail.com>
2023-08-27 20:05:57 +0530

    Update file error Handled

--- functions.h
+++ functions.h
@@ -1,11 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include<string.h>
+#include <string.h>
 
-//Read a file
+// Read a file
 void readFile(const char *file_name)
 {
-	FILE *fptr;
+    FILE *fptr;
     char in;
     fptr = fopen(file_name, "r");
     if (fptr == NULL)
@@ -21,50 +21,61 @@ void readFile(const char *file_name)
     fclose(fptr);
 }
 
-//Create a file
-void createFile(const char *file_name){
+// Create a file
+void createFile(const char *file_name)
+{
     FILE *fptr;
     char content[1000];
     fptr = fopen(file_name, "w");
-		if (fptr == NULL)
-		{
-			printf("File not found or unable to open.\n");
-		}
-		printf("\n\n\t\t\tWRITE MODE\n");
-		printf("Enter the content to write (Press Ctrl+Z or Ctrl+D to stop):\n");
-		while (fgets(content, sizeof(content), stdin) != NULL) {
-            // Remove the trailing newline character
-            int len = strlen(content);
-            if (len > 0 && content[len - 1] == '\n') {
-                content[len - 1] = '\0'; // Replace '\n' with '\0' (null character)
-            }
-            fputs(content, fptr);
+    if (fptr == NULL)
+    {
+        printf("File not found or unable to open.\n");
+    }
+    printf("\n\n\t\t\tWRITE MODE\n");
+    printf("Enter the content to write (Press Ctrl+Z or Ctrl+D to stop):\n");
+    while (fgets(content, sizeof(content), stdin) != NULL)
+    {
+        // Remove the trailing newline character
+        int len = strlen(content);
+        if (len > 0 && content[len - 1] == '\n')
+        {
+            content[len - 1] = '\0'; // Replace '\n' with '\0' (null character)
         }
-        fclose(fptr);
+        fputs(content, fptr);
+    }
+    fclose(fptr);
 }
 
-//Update a file
-void updateFile(const char *file_name){
+// Update a file
+void updateFile(const char *file_name)
+{
     FILE *fptr;
-    char in;
+    char content[1000];
     fptr = fopen(file_name, "a");
-		if (fptr == NULL)
-		{
-			printf("File not found or unable to open.\n");
-			exit(1);
-		}
-		printf("\n\n\t\t\tAPPEND MODE\n");
-		printf("Enter the content to append (Press Ctrl+Z or Ctrl+D to stop):\n");
-		while ((in = getchar()) != EOF)
-		{
-			putc(in, fptr);
-		}
-		fclose(fptr);
+    if (fptr == NULL)
+    {
+        printf("File not found or unable to open.\n");
+        exit(1);
+    }
+    printf("\n\n\t\t\tAPPEND MODE\n");
+    printf("Enter the content to append (Press Ctrl+Z or Ctrl+D to stop):\n");
+    while (fgets(content, sizeof(content), stdin) != NULL)
+    {
+        // Remove the trailing newline character
+        int len = strlen(content);
+        if (len > 0 && content[len - 1] == '\n')
+        {
+            content[len - 1] = '\0'; // Replace '\n' with '\0' (null character)
+        }
+        fputs(content, fptr);
+    }
+    fclose(fptr);
 }
 
-//Delete a file
-void deleteFile(const char *fileName){
-	int status = remove(fileName);
+// Delete a file
+void deleteFile(const char *fileName)
+{
+    int status = remove(fileName);
     if (status == 0)
     {
         printf("File deleted successfully.\n");
